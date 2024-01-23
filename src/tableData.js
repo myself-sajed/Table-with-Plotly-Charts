2d9f673a30c8cf137f2e206be245dc2b1bb5dbb7
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-01-23 19:48:20 +0530

    added mock data

---
+++ src/tableData.js
@@ -0,0 +1,124 @@
+const tableData = [
+    {
+        id: 1,
+        productName: "Apple MacBook Pro 17\"",
+        color: "Silver",
+        category: "Laptop",
+        accessories: "Yes",
+        available: "Yes",
+        price: "$2999",
+        weight: "3.0 lb.",
+        sales: 1200,
+        ratings: 4.5
+    },
+    {
+        id: 2,
+        productName: "Microsoft Surface Pro",
+        color: "White",
+        category: "Laptop PC",
+        accessories: "No",
+        available: "Yes",
+        price: "$1999",
+        weight: "1.0 lb.",
+        sales: 800,
+        ratings: 3.8
+    },
+    {
+        id: 3,
+        productName: "Magic Mouse 2",
+        color: "Black",
+        category: "Accessories",
+        accessories: "Yes",
+        available: "No",
+        price: "$99",
+        weight: "0.2 lb.",
+        sales: 300,
+        ratings: 4.0
+    },
+    {
+        id: 4,
+        productName: "Apple Watch",
+        color: "Black",
+        category: "Watches",
+        accessories: "Yes",
+        available: "No",
+        price: "$199",
+        weight: "0.12 lb.",
+        sales: 400,
+        ratings: 4.2
+    },
+    {
+        id: 5,
+        productName: "Apple iMac",
+        color: "Silver",
+        category: "PC",
+        accessories: "Yes",
+        available: "Yes",
+        price: "$2999",
+        weight: "7.0 lb.",
+        sales: 1500,
+        ratings: 4.8
+    },
+    {
+        id: 6,
+        productName: "Apple AirPods",
+        color: "White",
+        category: "Accessories",
+        accessories: "No",
+        available: "Yes",
+        price: "$399",
+        weight: "38 g",
+        sales: 1000,
+        ratings: 4.6
+    },
+    {
+        id: 7,
+        productName: "iPad Pro",
+        color: "Gold",
+        category: "Tablet",
+        accessories: "No",
+        available: "Yes",
+        price: "$699",
+        weight: "1.3 lb.",
+        sales: 900,
+        ratings: 4.4
+    },
+    {
+        id: 8,
+        productName: "Magic Keyboard",
+        color: "Black",
+        category: "Accessories",
+        accessories: "Yes",
+        available: "Yes",
+        price: "$99",
+        weight: "453 g",
+        sales: 2000,
+        ratings: 4.9
+    },
+    {
+        id: 9,
+        productName: "Apple TV 4K",
+        color: "Black",
+        category: "TV",
+        accessories: "Yes",
+        available: "No",
+        price: "$179",
+        weight: "1.78 lb.",
+        sales: 600,
+        ratings: 4.1
+    },
+    {
+        id: 10,
+        productName: "AirTag",
+        color: "Silver",
+        category: "Accessories",
+        accessories: "Yes",
+        available: "No",
+        price: "$29",
+        weight: "53 g",
+        sales: 350,
+        ratings: 3.9
+    }
+];
+
+export default tableData
